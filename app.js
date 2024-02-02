617259997709cdc63f3e271aac3b9bc375346654
Naman Soni <namansoni@Namans-MacBook-Pro.local>
2024-02-02 09:21:51 +0530

    Deployment

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const MongoStore = require('connect-mongo');
+
+const dbUrl = process.env.DB_URL
+
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+
+// 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -40,9 +47,22 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret',
+    }
+});
+
+store.on("error", function(e){
+    console.log("SESSION STORE ERROR!", e);
+})
+
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret: 'thisshouldbeabettersecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
